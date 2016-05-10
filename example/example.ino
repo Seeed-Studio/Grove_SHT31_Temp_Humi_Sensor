b6afd70fc13a5fe910e08be52422153073b9954c
lanselambor <757016515@qq.com>
2016-05-10 15:18:52 +0800

    initialize

---
+++ example/example.ino
@@ -0,0 +1,26 @@
+#include <Arduino.h>
+#include <Wire.h>
+#include "SHT31.h"
+
+SHT31 sht31 = SHT31();
+
+void setup() {  
+  Serial.begin(9600);
+  while(!Serial);
+  Serial.println("begin...");  
+  sht31.begin();  
+}
+
+void loop() {
+  float temp = sht31.getTemperature();
+  float hum = sht31.getHumidity();
+  Serial.print("Temp = "); 
+  Serial.print(temp);
+  Serial.println(" C"); 
+  Serial.print("Hum = "); 
+  Serial.print(hum);
+  Serial.println("%"); 
+  Serial.println();
+  delay(1000);
+}
+
